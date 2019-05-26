b289ad9781b6febc02ec56aee5566b5d8f137ba2
Jenny Q <jenny.q@me.com>
2019-05-26 18:33:02 -0400

    Initial Commit
    Setting up image files, icon files, storyboard

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by JennyQ on 5/26/19.
+//  Copyright © 2019 Jennifer Quispe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
